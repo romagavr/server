1be7e4a6f9756c21fc8f25038e8c87c9ae0af743
Роман Гавриленко <gavrilenko@denero.ru>
2020-08-14 14:33:13 +0300

    Add client code

---
+++ client/client.c
@@ -0,0 +1,54 @@
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<netdb.h>
+#include<unistd.h>
+#include<errno.h>
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<time.h>
+
+int main(int argc, char *argv[]){
+    if (argc < 3) {
+        fprintf(stderr, "usage: tcp_client hostname port\n");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Configuring remote address...\n");
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_socktype = SOCK_STREAM;
+    struct addrinfo *peer_address;
+    if (getaddrinfo(argv[1], argv[2], &hints, &peer_address)) {
+        fprintf(stderr, "geraddrinfo() failed. (%d)\n", errno);
+        exit(EXIT_FAILURE);
+    }
+   
+    printf("Remote address is: ");
+    char address_buffer[100];
+    char service_buffer[100];
+    getnameinfo(peer_address->ai_addr, peer_address->ai_addrlen, address_buffer, sizeof(address_buffer), service_buffer, sizeof(service_buffer), NI_NUMERICHOST);
+    printf("%s %s\n", address_buffer, service_buffer);
+
+    printf("Creating socket...\n");
+    int socket_peer = socket(peer_address->ai_family, peer_address->ai_socktype, peer_address->ai_protocol);
+    if (socket_peer < 0) {
+        fprintf(stderr, "socket() failed. (%d)\n", errno);
+        exit(EXIT_FAILURE);
+    }
+    
+    printf("Connecting...\n");
+    if (connect(socket_peer, peer_address->ai_addr, peer_address->ai_addrlen)) {
+        fprintf(stderr, "connect() failed. (%d)\n", errno);
+        exit(EXIT_FAILURE);
+    }
+    freeaddrinfo(peer_address);
+
+    printf("Connected.\n");
+    printf("To send data, enter text followed by enter.\n");
+
+	return 0;
+}
